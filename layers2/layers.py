268e9efa57e8da84201da37fd55f8577fcbea2c4
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-11-06 21:06:35 +0000

    Update layers.py

--- layers2/layers.py
+++ layers2/layers.py
@@ -16,13 +16,15 @@ class ActivationDistributionsPlugin(PluginBase):
     def __init__(self, app, session_state):
         # Load the plugin configuration
         config = self._get_config()
+        template_folder = os.path.join(os.path.dirname(__file__), 'templates')
+        
         super().__init__(
             app,
             session_state,
             name=config['name'],
             import_name=__name__,
             url_prefix=f"/{config['name']}",
-            template_folder='templates'
+            template_folder=template_folder
         )
         self._model_info = None
 
