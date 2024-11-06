509dbf12888f30493b2a8ac4a228349ac864ae2d
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-11-06 19:08:33 +0000

    Delete plugins directory

--- plugins/stats/static/default.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Retrieve and display the model info
-    const modelInfo = {{ model_info | tojson | safe }};
-    const modelInfoContent = document.getElementById('model-info-content');
-    modelInfoContent.textContent = JSON.stringify(modelInfo, null, 2);
-    
-    // Initialize Materialize CSS tooltips
-    M.Tooltip.init(document.querySelectorAll('.tooltip'));
-});
